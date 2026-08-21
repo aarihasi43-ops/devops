0a89b2edfcc514d3b83d37bf0a3c3d8fc0cdb9e8
Ubuntu <ubuntu@ip-172-31-13-46.ap-south-2.compute.internal>
2026-08-21 04:45:58 +0000

    hs

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello,Hello World!");
-System.out.println("hello  helo ooooooHello World!");
+System.out.println("hello world Hello World!");
  
 
     }
